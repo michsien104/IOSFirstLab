171f3971dcf8c3db7384589ffe9ab8c8eedf9f63
Student <student@imac-16.local>
2019-12-13 10:24:11 +0100

    initial commit

---
+++ MS1Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MS1Lab
+//
+//  Created by Student on 13/12/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
